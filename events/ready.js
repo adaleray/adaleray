b5d4577bec499f60a721607e797f2d7bbe27ec30
Glitch (sasaqweawe) <none>
2020-07-07 08:13:57 +0000

    🚡😇 Checkpoint ./events/ready.js:17406785/35

--- events/ready.js
+++ events/ready.js
@@ -8,7 +8,7 @@ var prefix= ayarlar.prefix;
 module.exports = client => {
   console.log(`${client.guilds.size} Kadar Sunucuya Hizmet Veriyorum!`);
   client.user.setStatus("idle");
-    client.user.setActivity("Lexium Bot ❤ | +davet", { type: "WATCHING" });
+    client.user.setActivity("Boş Altyapı :D", { type: "WATCHING" });
   
 
   
